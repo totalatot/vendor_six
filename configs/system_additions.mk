a324ad14414da060455d6b5e2b5938747e84c83c
R3Ds <fpoolboy@gmail.com>
2017-09-08 01:54:04 -0400

    prebuilt: Add Viper4Arise support

--- configs/system_additions.mk
+++ configs/system_additions.mk
@@ -64,4 +64,36 @@ EXCLUDE_SYSTEMUI_TESTS := true
     #audio_effects.conf
 #endif
 
+# Viper4Rise 
+PRODUCT_COPY_FILES += \
+    vendor/six/prebuilt/lib/libauaudioutils.so:system/lib/libauaudioutils.so \
+    vendor/six/prebuilt/lib/libaudsd2pcm.so:system/lib/libaudsd2pcm.so \
+    vendor/six/prebuilt/lib/libldacBT_enc.so:system/lib/libldacBT_enc.so \
+    vendor/six/prebuilt/lib/libsrsprocessing.so:system/lib/libsrsprocessing.so \
+    vendor/six/prebuilt/lib/libgnmc_decoder.1.1.8.so:system/lib/libgnmc_decoder.1.1.8.so \
+    vendor/six/prebuilt/lib/libausndfile.so:system/lib/libausndfile.so \
+    vendor/six/prebuilt/lib/libausoxr.so:system/lib/libausoxr.so \
+    vendor/six/prebuilt/lib/libauusb.so:system/lib/libauusb.so \
+    vendor/six/prebuilt/lib/libauogg.so:system/lib/libauogg.so \
+    vendor/six/prebuilt/lib/libgnsdk_dsp.3.09.0.so:system/lib/libgnsdk_dsp.3.09.0.so \
+    vendor/six/prebuilt/lib/libautaglib.so:system/lib/libautaglib.so \
+    vendor/six/prebuilt/lib/libmaxxeffectwrapper.so:system/lib/libmaxxeffectwrapper.so \
+    vendor/six/prebuilt/lib/libV4AJniUtils.so:system/lib/libV4AJniUtils.so \
+    vendor/six/prebuilt/lib/libMAM2-processcode-Coretex_A9.so:system/lib/libMAM2-processcode-Coretex_A9.so \
+    vendor/six/prebuilt/lib/libMAM2-wavesfx-Coretex_A9.so:system/lib/libMAM2-wavesfx-Coretex_A9.so \
+    vendor/six/prebuilt/lib/libosl-maxxaudio-itf.so:system/lib/libosl-maxxaudio-itf.so \
+    vendor/six/prebuilt/lib/libauvorbis.so:system/lib/libauvorbis.so \
+    vendor/six/prebuilt/lib/libgnustl_shared.so:system/lib/libgnustl_shared.so \
+    vendor/six/prebuilt/lib/libradsone-shared.so:system/lib/libradsone-shared.so \
+    vendor/six/prebuilt/lib/libambisonic_audio_renderer.so:system/lib/libambisonic_audio_renderer.so \
+    vendor/six/prebuilt/lib/libswresample-2.so:system/lib/libswresample-2.so \
+    vendor/six/prebuilt/lib/libaemagic.so:system/lib/libaemagic.so \
+    vendor/six/prebuilt/lib/soundfx/libeffectproxy.so:system/lib/soundfx/libeffectproxy.so \
+    vendor/six/prebuilt/lib/soundfx/libmaxxeffect-cembedded.so:system/lib/soundfx/libmaxxeffect-cembedded.so \
+    vendor/six/prebuilt/lib/soundfx/libqcompostprocbundle.so:system/lib/soundfx/libqcompostprocbundle.so \
+    vendor/six/prebuilt/lib/soundfx/libsonypostprocbundle.so:system/lib/soundfx/libsonypostprocbundle.so \
+    vendor/six/prebuilt/lib/soundfx/libsonysweffect.so:system/lib/soundfx/libsonysweffect.so \
+    vendor/six/prebuilt/lib/soundfx/libv4a_fx_ics.so:system/lib/soundfx/libv4a_fx_ics.so \
+    vendor/six/prebuilt/lib/soundfx/libvisualizer.so:system/lib/soundfx/libvisualizer.so
+
 $(call inherit-product-if-exists, vendor/six/prebuilt/prebuilt.mk)
