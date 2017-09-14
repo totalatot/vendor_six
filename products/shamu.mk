f7731c0b1a4d6e98fd9ec003f327cb04e9c1fdd6
R3Ds <fpoolboy@gmail.com>
2017-09-14 16:39:21 -0400

    shamu: Update fingerprint shamu:7.1.1/NGI55D/4298276:user/release-keys

--- products/shamu.mk
+++ products/shamu.mk
@@ -28,6 +28,6 @@ PRODUCT_MANUFACTURER := motorola
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N8I11B/4171878:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 8.0.0 OPR6.170623.013 4283548 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/NGI55D/4298276:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 8.0.0 OPR6.170623.017 4298423 release-keys"
 
