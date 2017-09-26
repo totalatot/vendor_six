543bb1f008901792143429e1cfb752daa016c616
R3Ds <fpoolboy@gmail.com>
2017-09-26 11:01:45 -0400

    version: Update to v1.6

--- configs/version.mk
+++ configs/version.mk
@@ -14,7 +14,7 @@
 
 #SiX versioning
 ifndef SIX_BUILD_TYPE
-    SIX_BUILD_TYPE := OPR6-v1.5-R3Ds
+    SIX_BUILD_TYPE := OPR6-v1.6-R3Ds
 endif
 
 SIX_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d)-$(SIX_BUILD_TYPE)
