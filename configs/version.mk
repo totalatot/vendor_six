a144ca4bac66ae3cd03d57451faf49501db94950
R3Ds <fpoolboy@gmail.com>
2017-10-05 13:57:55 -0400

    version: Update to OPR5-v1.7

--- configs/version.mk
+++ configs/version.mk
@@ -14,7 +14,7 @@
 
 #SiX versioning
 ifndef SIX_BUILD_TYPE
-    SIX_BUILD_TYPE := OPR6-v1.7-R3Ds
+    SIX_BUILD_TYPE := OPR5-v1.7-R3Ds
 endif
 
 SIX_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d)-$(SIX_BUILD_TYPE)
