9b6f9e39e27082950eb8adeb08aaae885c3319d0
R3Ds <fpoolboy@gmail.com>
2017-10-14 07:07:04 -0400

    fixes: Add back persist.sys.disable_rescue

--- configs/aosp_fixes.mk
+++ configs/aosp_fixes.mk
@@ -23,8 +23,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=false \
     ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
     ro.setupwizard.rotation_locked=true \
-    ro.opa.eligible_device=true
-    #    persist.sys.disable_rescue=true
+    ro.opa.eligible_device=true \
+    persist.sys.disable_rescue=true
     #    ro.control_privapp_permissions=enforce \
 
 # Proprietary latinime libs needed for Keyboard swyping
